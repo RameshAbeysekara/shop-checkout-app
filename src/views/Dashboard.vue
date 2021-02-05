7c9ebbeed347b76c1aa4f77aca295b6bcd5bfffe
Ramesh Abeysekara <rameshabeysekara@gmail.com>
2021-02-05 08:46:42 +0530

    add order view in dashboard

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,6 +1,28 @@
 <template>
   <div class="dashboard">
-    <h1>Order Summary{{ this.logedInUser }}</h1>
+    <h1>Orders List ({{ this.logedInUser }} user)</h1>
+    <div v-for="order in this.orders" :key="order.index">
+      <base-card>
+        <h3>Item Details</h3>
+        <p>Item ID: {{ order.orderDetails.item.id }}</p>
+        <p>Color: {{ order.orderDetails.item.color }}</p>
+        <p>Size: {{ order.orderDetails.item.size }}</p>
+        <p>Quantity: {{ order.orderDetails.item.quantity }}</p>
+
+        <h3>Shipping Details</h3>
+        <p>First Name: {{ order.orderDetails.shipping.firstName }}</p>
+        <p>Last Name: {{ order.orderDetails.shipping.lastName }}</p>
+        <p>Phone Number: {{ order.orderDetails.shipping.phoneNumber }}</p>
+        <p>Address Line 1: {{ order.orderDetails.shipping.addressLine1 }}</p>
+        <p>Address Line 2: {{ order.orderDetails.shipping.addressLine2 }}</p>
+
+        <h3>Payment Details</h3>
+        <p>Card Number: {{ order.orderDetails.payment.cardNumber }}</p>
+        <p>Expire Date: {{ order.orderDetails.payment.expireDate }}</p>
+        <p>CV Code: {{ order.orderDetails.payment.cvCode }}</p>
+        <p>Card Name: {{ order.orderDetails.payment.cardName }}</p>
+      </base-card>
+    </div>
   </div>
 </template>
 
@@ -10,12 +32,27 @@ export default {
   data: function() {
     return {
       logedInUser: "",
+      orders: [],
     };
   },
   mounted() {
     if (localStorage.user) {
       this.logedInUser = localStorage.user;
-      console.log("orders", localStorage.orders);
+      JSON.parse(localStorage.orders).map((order, index) => {
+        if (this.logedInUser == "admin") {
+          this.orders.push({
+            index: index,
+            orderDetails: order.orderDetails,
+          });
+        } else {
+          if (this.logedInUser == order.user) {
+            this.orders.push({
+              index: index,
+              orderDetails: order.orderDetails,
+            });
+          }
+        }
+      });
     }
   },
 };
